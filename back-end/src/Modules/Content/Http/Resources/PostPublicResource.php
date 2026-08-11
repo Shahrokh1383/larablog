e59536be801051b4ed8810178a28d8519ff33b42
Shahrokh <shahrokh.hatami2004@gmail.com>
2026-08-11 18:39:42 +0330

    feat(content): expose is_editors_pick in public post API
    - Add is_editors_pick field to PostPublicResource so frontend can display Editor's Pick badges on public pages

--- back-end/src/Modules/Content/Http/Resources/PostPublicResource.php
+++ back-end/src/Modules/Content/Http/Resources/PostPublicResource.php
@@ -21,6 +21,7 @@ class PostPublicResource extends JsonResource
             'comments_count' => $this->comments_count ?? 0,
             'is_saved'       => $this->is_saved ?? false,
             'published_at'   => $this->published_at,
+            'is_editors_pick'=> $this->is_editors_pick,
             'category'       => new CategoryResource($this->whenLoaded('category')),
             'tags'           => TagResource::collection($this->whenLoaded('tags')),
             'author'         => $this->when(isset($this->author), $this->author),
