6b3242d88d0091a184be900d1bd35a5fc653c23c
Shahrokh <shahrokh.hatami2004@gmail.com>
2026-08-30 03:50:01 +0330

    change databaseseeder.php for comment seeding

--- back-end/database/seeders/DatabaseSeeder.php
+++ back-end/database/seeders/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             ProfileSeeder::class,
             ContentSeeder::class,
-            //CommentSeeder::class,
+            CommentSeeder::class,
             SavedPostSeeder::class,
             PostReadSeeder::class,
             SubscriberSeeder::class,
